5b239c0f97573af7f8925b69b0079ada01da969d
shivam5676 <ssultanpura9@gmail.com>
2023-04-25 08:51:36 -0700

    msg willl be displayed if there is no expense

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,31 +7,51 @@ import ExpensesFilter from "./ExpensesFilter";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filteredExpenses=props.items.filter((expense)=>{
-    return expense.date.getFullYear().toString()===filteredYear
-  })
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  let expensecontent = <p>no content found</p>;
+  if (filteredExpenses.length === 1) {
+    expensecontent = filteredExpenses.map((expense) => {
+      return (
+        <div>
+          <Expenseitem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.location}
+          />
+          <p>"Only single Expense here. Please add more..."</p>
+        </div>
+      );
+    });
+  }
 
+  if (filteredExpenses.length > 1) {
+    expensecontent = filteredExpenses.map((expense) => {
+      return (
+        <Expenseitem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      );
+    });
+  }
   return (
     <div>
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense)=>{
-        return <Expenseitem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-       location={expense.location}
-      />
-      })}
-    
-      
+      {expensecontent}
     </div>
   );
 };
